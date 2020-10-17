fc249817cd0794283306c3a4b521b1b3ca8f518e
Kic4k <lukaskickic@gmail.com>
2020-10-17 20:17:27 +0200

    added all tasks

--- L2.scala
+++ L2.scala
@@ -13,7 +13,30 @@ listIlo(List())
 listIlo(List(-3,2,1))
 listIlo(List(1,2,3,0))
 
+// 2
+def list2sentence (list:List[String],eol:String,delimiter:String):String = list match {
+	case Nil => eol
+	case head::Nil => head + eol
+	case head::tail => head + delimiter + list2sentence(tail,eol,delimiter)
+}
 
+// 2 test
+println(list2sentence(List("Another","pointless","test","case"),".",", "))
+println(list2sentence(List("Pointless"),".",", "))
+println(list2sentence(List(),".",", "))
+
+// 3
+def checkInter (list:List[Int],X:Int,Y:Int):Boolean = list match {
+	case Nil => true
+	case head::Nil => if(head>=X && head<=Y) true else false
+	case head::tail => checkInter (tail,X,Y)
+}
+
+// 3 test
+checkInter(List(1,2),3,4)
+checkInter(List(1,2),0,4)
+checkInter(List(),0,4)
+checkInter(List(5),0,4)
 
 // 4
 def potegaI(X: Int, Y: Int): Int = {
